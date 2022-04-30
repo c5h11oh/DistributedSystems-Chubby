608de3486920054de269088d1ab98c3689802024
Chang-Yen Tseng <10328757+Summon528@users.noreply.github.com>
2022-04-29 22:11:02 -0500

    add python binding
    currently only work when built in release mode

---
+++ pyclientlib.cpp
@@ -0,0 +1,21 @@
+#include <pybind11/functional.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+#include <optional>
+
+#include "clientlib.cpp"
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(pyclientlib, m) {
+  py::class_<SkinnyClient>(m, "SkinnyClient")
+      .def(py::init())
+      .def("Open", &SkinnyClient::Open, py::arg("path"),
+           py::arg("cb") = std::nullopt)
+      .def("SetContent", &SkinnyClient::SetContent)
+      .def("GetContent", &SkinnyClient::GetContent)
+      .def("TryAcquire", &SkinnyClient::TryAcquire)
+      .def("Acquire", &SkinnyClient::Acquire)
+      .def("Release", &SkinnyClient::Release);
+}
